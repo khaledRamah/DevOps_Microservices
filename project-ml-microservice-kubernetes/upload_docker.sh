5dc55ea80d501b9eae753ada8aa94ebdc131cfcc
Ubuntu <ubuntu@ip-172-31-6-169.us-west-2.compute.internal>
2020-07-07 17:07:06 +0000

    push to docker hub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+ dockerpath=khaledgamalelsayed/api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag api $dockerpath
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
